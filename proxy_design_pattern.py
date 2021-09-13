4013c079e2488077ba5a5b0ec911f89e27b29973
mokeam <jumokemariam@gmail.com>
2021-09-13 20:11:16 +0200

    Added Proxy Design Pattern

---
+++ proxy_design_pattern.py
@@ -0,0 +1,25 @@
+from abc import ABCMeta, abstractstaticmethod
+
+class IPerson(metaclass = ABCMeta):
+
+	@abstractstaticmethod
+	def person_method():
+		""" Interface Method """
+
+class Person(IPerson):
+
+	def person_method(self):
+		print("I am a person!")
+
+class ProxyPerson(IPerson):
+
+	def __init__(self):
+		self.person = Person()
+
+	def person_method(self):
+		print("I am the proxy functionality")
+		self.person.person_method()
+
+if __name__ == '__main__':
+	p = ProxyPerson()
+	p.person_method()
